dd09c321e57335d36a18d44381b7bf267f4a7316
cleexiang <cleexiang@126.com>
2017-09-20 20:05:40 +0800

    Initial Commit

---
+++ SwifterExtensions/SwifterExtensions.h
@@ -0,0 +1,19 @@
+//
+//  SwifterExtensions.h
+//  SwifterExtensions
+//
+//  Created by lixiang on 2017/9/20.
+//  Copyright © 2017年 lixiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwifterExtensions.
+FOUNDATION_EXPORT double SwifterExtensionsVersionNumber;
+
+//! Project version string for SwifterExtensions.
+FOUNDATION_EXPORT const unsigned char SwifterExtensionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwifterExtensions/PublicHeader.h>
+
+
